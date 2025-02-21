a1c84b8a0194765726d43213708c04d3ce84a545
savard <tom.savard@orange.fr>
2025-02-21 01:38:52 +0100

    maj de nussinov.hpp

--- code/c++/nussinov.hpp
+++ code/c++/nussinov.hpp
@@ -1,10 +1,20 @@
 #ifndef NUSSINOV_HPP
 #define NUSSINOV_HPP
 
+
+#include <vector>
+#include <string>
+#include <utility>
+
 #include "utilities.hpp"
 
+// Déclaration de la fonction FillMatrix
+void FillMatrix(const std::string& sequence, std::vector<std::vector<int>>& m, int theta = 3, int pair_energy = ::pair_energy);
+
+float Nussinov(const std::string& sequence,int start_index, int end_index, int theta = 3, int pair_energy = ::pair_energy);
 
 
+// Déclaration de la fonction Backtrack
+void Backtrack(int i, int j, const std::vector<std::vector<int>>& m, const std::string& sequence, std::vector<std::pair<int, int>>& S, int theta = 3);
 
-
-#endif //NUSSINOV_HPP
+#endif // NUSSINOV_HPP
