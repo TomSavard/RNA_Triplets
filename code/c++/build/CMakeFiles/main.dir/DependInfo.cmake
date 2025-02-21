dde34a10d1dc0f6b0c8e680a8409b06fb6122757
savard <tom.savard@orange.fr>
2025-02-21 14:50:20 +0100

    ignore the build directory

---
+++ code/c++/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/savardtom/Desktop/INF551/RNA_Triplets/code/c++/global_variables.cpp" "CMakeFiles/main.dir/global_variables.cpp.o" "gcc" "CMakeFiles/main.dir/global_variables.cpp.o.d"
+  "/Users/savardtom/Desktop/INF551/RNA_Triplets/code/c++/nussinov.cpp" "CMakeFiles/main.dir/nussinov.cpp.o" "gcc" "CMakeFiles/main.dir/nussinov.cpp.o.d"
+  "/Users/savardtom/Desktop/INF551/RNA_Triplets/code/c++/utilities.cpp" "CMakeFiles/main.dir/utilities.cpp.o" "gcc" "CMakeFiles/main.dir/utilities.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
