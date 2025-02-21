6a73c2b93e84c6d5f23e877a2664ecff118fdcb0
savard <tom.savard@orange.fr>
2025-02-21 14:40:44 +0100

    update and adding the function Nussinov which simply returns the value of the energy instead of the filled matrix

--- code/c++/nussinov.cpp
+++ code/c++/nussinov.cpp
@@ -2,13 +2,13 @@
 #include "utilities.hpp"
 #include "global_variables.hpp"
 
-// #include <ViennaRNA/PS_rna_plot.h>
+
 #include <iostream>
 #include <vector>
 #include <string>
 
 
-void FillMatrix(const std::string& sequence, std::vector<std::vector<int>>& m, int theta = 3, int pair_energy = ::pair_energy) {
+void FillMatrix(const std::string& sequence, std::vector<std::vector<int>>& m, int theta, int pair_energy) {
     /**
      * @brief Fills the minimum energy matrix 
      * 
@@ -49,7 +49,7 @@ void FillMatrix(const std::string& sequence, std::vector<std::vector<int>>& m, i
 }
 
 
-void Backtrack(int i, int j, const std::vector<std::vector<int>>& m, const std::string& sequence, std::vector<std::pair<int, int>>& S, int theta = 3){
+void Backtrack(int i, int j, const std::vector<std::vector<int>>& m, const std::string& sequence, std::vector<std::pair<int, int>>& S, int theta){
     /**
      * @brief Backtrack the minimum energy matrix to find the secondary structure
      * 
@@ -94,6 +94,49 @@ void Backtrack(int i, int j, const std::vector<std::vector<int>>& m, const std::
 }
 
 
+float Nussinov(const std::string& sequence,int start_index, int end_index, int theta, int pair_energy) {
+    /**
+     * @brief Fills the minimum energy matrix 
+     * 
+     * Detailled description : TO BE DONE
+     * 
+     * @param sequence : the RNA sequence
+     * @param start_index : the first index of the subsequence
+     * @param end_index : the last index of the subsequence
+     * @param theta : the minimum distance between paired bases
+     * @param pair_energy : the energy of a pair
+     * 
+     * @return void
+     */
+    std::vector<std::vector<int>> m;
+    std::cout << "  Start of Nussinov" << std::endl;
+    int len_seq = sequence.size();
+
+    // Initialize the matrix with zeros.
+    m.assign(len_seq, std::vector<int>(len_seq, 0));
+
+    // Fill the matrix
+    for (int i = len_seq - 1; i >= 0; i--) {
+        for (int j = i + theta + 1; j < len_seq; j++) {
+            // Case A : pos i without partner
+            m[i][j] = m[i + 1][j];
+            // Case B : pos i and j form a base pair
+            if (can_pair(sequence[i], sequence[j])) {
+                m[i][j] = std::min(m[i][j], m[i + 1][j - 1] + pair_energy);
+            }
+            // Case C : pairing with another base at index k
+            for (int k = i + theta + 1; k < j; k++) {
+                if (can_pair(sequence[i], sequence[k])) {
+                    m[i][j] = std::min(m[i][j], m[i + 1][k - 1] + m[k + 1][j] + pair_energy);
+                }
+            }
+        }
+    }
+
+
+    std::cout << "  End of Nussinov" << std::endl;
+    return m[start_index][end_index];
+}
 
 
 #ifdef NUSSINOV_TEST
@@ -121,9 +164,8 @@ int main() {
         std::cout << "(" << pair.first << ", " << pair.second << ") ";
     }
     std::cout << std::endl;
-    
-    // PS_rna_plot(sequence.c_str(), dot_bracket_structure.c_str(), "RNA_plot.ps");
 
     return 0;
 }
 #endif //NUSSINOV_TEST
+
