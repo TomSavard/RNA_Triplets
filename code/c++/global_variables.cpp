65210285068022a302022b6346a833d1f18dc74d
savard <tom.savard@orange.fr>
2025-03-25 23:18:28 +0100

    adding the inf_energy variable to it

--- code/c++/global_variables.cpp
+++ code/c++/global_variables.cpp
@@ -4,6 +4,8 @@ const int theta = 1; //! should be set to 3
 
 const float pair_energy = -1;
 
+const float inf_energy = std::numeric_limits<float>::infinity();
+
 const std::vector< std::pair<char, char> > possible_pairs = {
     {'A','U'}, {'U','A'}, {'G','C'}, {'C','G'}, {'G','U'}, {'U','G'}
 
--- code/c++/global_variables.hpp
+++ code/c++/global_variables.hpp
@@ -8,6 +8,8 @@ extern const int theta;
 
 extern const float pair_energy;
 
+extern const float inf_energy;
+
 extern const std::vector< std::pair<char, char> > possible_pairs;
 
 #endif // GLOBAL_VARIABLES_HPP
