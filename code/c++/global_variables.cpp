dc77379fe5957d7772f2b64e0e734003a6b1dcd0
savard <tom.savard@orange.fr>
2025-03-27 17:34:13 +0100

    adding comments

--- code/c++/global_variables.cpp
+++ code/c++/global_variables.cpp
@@ -1,8 +1,17 @@
+/**
+ * @file global_variables.cpp
+ * @brief Implements global constants for RNA structure calculations.
+ *
+ * This file defines constants for RNA secondary structure prediction.
+ * These constants are used for energy calculations and base pairing.
+ * 
+ */
+
 #include "global_variables.hpp"
 
-const int theta = 1; //! should be set to 3 
+const int theta = 1; // Usually set to 3
 
-const float pair_energy = -1;
+const float pair_energy = -1; // Can be changed to a dictionary pair/energy. + define a function to extract the energy of a specific pair.
 
 const float inf_energy = std::numeric_limits<float>::infinity();
 
--- code/c++/global_variables.hpp
+++ code/c++/global_variables.hpp
@@ -1,15 +1,45 @@
+/**
+ * @file global_variables.hpp
+ * @brief Declares global constants for RNA structure calculations.
+ *
+ * This file contains global constants for RNA structure prediction, including
+ * energy values and valid base pair combinations.
+ *
+ * @date 2025-03
+ * @version 1.0
+ */
+
 #ifndef GLOBAL_VARIABLES_HPP
 #define GLOBAL_VARIABLES_HPP
 
 #include <vector>
 #include <utility>
 
+/**
+ * @brief Minimum distance between paired bases. (0 = no constraint)
+ */
 extern const int theta;
 
+
+/**
+ * @brief Energy of a base pair.
+ */
 extern const float pair_energy;
 
+
+/**
+ * @brief Energy value representing infinity.
+ */
 extern const float inf_energy;
 
+
+/**
+ * @brief Valid base pair combinations.
+ *
+ * This vector contains pairs of characters representing valid base pairs in RNA.
+ * Watson-Crick + Wobble pairs are included.
+ */
 extern const std::vector< std::pair<char, char> > possible_pairs;
+
 
 #endif // GLOBAL_VARIABLES_HPP
